061e3c42a85b2cb37986339f8b41dc7f99458a72
lordzuko <himanshumaurya2214225@hotmail.com>
2016-12-13 17:21:20 +0530

    Updated Readme and fixed table creation issue

--- cloud/apps.py
+++ cloud/apps.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-from django.apps import AppConfig
-
-
-class CloudConfig(AppConfig):
-    name = 'cloud'
+# from django.apps import AppConfig
+#
+#
+# class CloudConfig(AppConfig):
+#     name = 'cloud'
